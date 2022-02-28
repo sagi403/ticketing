a6c9e3ceb56374e02a06b9bbdb44848255ac0740
Sagi <sagi403@gmail.com>
2022-02-28 14:29:05 +0200

    User creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -22,11 +22,17 @@ router.post(
     }
 
     const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
+    if (existingUser) {
+      console.log("Email in use");
+      return res.send({});
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
