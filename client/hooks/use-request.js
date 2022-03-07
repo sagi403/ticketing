6d4253d8ea2e224033b6f5d0aad2754993dce664
Sagi <sagi403@gmail.com>
2022-03-07 15:19:50 +0200

    Creating and using the useRequest hook

---
+++ client/hooks/use-request.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const useRequest = ({ url, method, body }) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
+      return response.data;
+    } catch (err) {
+      setErrors(
+        <div className="alert alert-danger">
+          <h4>Oops...</h4>
+          <ul className="my-0">
+            {err.response.data.errors.map(error => (
+              <li key={error.message}>{error.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return { doRequest, errors };
+};
+
+export default useRequest;
