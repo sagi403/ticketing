a65c1aaba32ef88b7d0d223e440374f56d533ff4
Sagi <sagi403@gmail.com>
2022-03-08 14:12:19 +0200

    Specifying the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,29 @@
 import React from "react";
+import axios from "axios";
 
-const index = () => {
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
 
-export default index;
+LandingPage.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    // We are on the server!
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+    return data;
+  } else {
+    // We are on the browser!
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+};
+
+export default LandingPage;
