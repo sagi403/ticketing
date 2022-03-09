e62d6dfcaad7d8c69bb7abc8290ebec236cad770
Sagi <sagi403@gmail.com>
2022-03-09 11:59:10 +0200

    Conditionally showing links

--- client/components/header.js
+++ client/components/header.js
@@ -2,6 +2,22 @@ import React from "react";
 import Link from "next/link";
 
 const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter(linkConfig => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <Link href={href}>
+            <a className="nav-link">{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
   return (
     <nav className="navbar navbar-light bg-light p-2">
       <Link href="/">
@@ -9,9 +25,7 @@ const Header = ({ currentUser }) => {
       </Link>
 
       <div className="d-flex justify-content-end p-2">
-        <ul className="nav d-flex align-items-center">
-          {currentUser ? "Sign out" : "Sign in/up"}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
