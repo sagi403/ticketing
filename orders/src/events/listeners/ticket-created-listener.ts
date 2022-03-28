a9ebf86cd976ab466d741bbc649d977409d58ccc
Sagi <sagi403@gmail.com>
2022-03-28 14:07:26 +0300

    Blueprint for listeners

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,9 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketCreatedEvent } from "@slticketing/common";
+import { Ticket } from "../../models/ticket";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+  queueGroupName = "orders-service";
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {}
+}
