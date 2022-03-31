63da5e0d61e2389ebba7cb9a1fd0bc0b12448539
Sagi <sagi403@gmail.com>
2022-03-31 15:29:47 +0300

    Order canceled listener

---
+++ tickets/src/events/listeners/order-canceled-listener.ts
@@ -0,0 +1,32 @@
+import { Message } from "node-nats-streaming";
+import { Listener, OrderCanceledEvent, Subjects } from "@slticketing/common";
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+
+export class OrderCanceledListener extends Listener<OrderCanceledEvent> {
+  readonly subject = Subjects.OrderCanceled;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCanceledEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ orderId: undefined });
+    await ticket.save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    });
+
+    msg.ack();
+  }
+}
