3d1b3d1eb5636616edb61fc2833c219b209afc5d
Sagi <sagi403@gmail.com>
2022-04-02 14:15:19 +0300

    Emitting the order canceled event

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -6,6 +6,7 @@ import {
 } from "@slticketing/common";
 import { Message } from "node-nats-streaming";
 import { Order } from "../../models/order";
+import { OrderCanceledPublisher } from "../publishers/order-canceled-publisher";
 import { queueGroupName } from "./queue-group-name";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
@@ -13,7 +14,7 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
   queueGroupName = queueGroupName;
 
   async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate("ticket");
 
     if (!order) {
       throw new Error("Order not found");
@@ -22,5 +23,16 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     order.set({
       status: OrderStatus.Canceled,
     });
+    await order.save();
+
+    await new OrderCanceledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
   }
 }
