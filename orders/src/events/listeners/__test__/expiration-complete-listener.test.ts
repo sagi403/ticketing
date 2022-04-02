b2820b8880d9644447da30a8692b3539d2f6641a
Sagi <sagi403@gmail.com>
2022-04-02 14:25:57 +0300

    Initial setup for the expiration complete listener

---
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { ExpirationCompleteEvent, OrderStatus } from "@slticketing/common";
+import { Order } from "../../../models/order";
+import { Ticket } from "../../../models/ticket";
+import { natsWrapper } from "../../../nats-wrapper";
+import { ExpirationCompleteListener } from "../expiration-complete-listener";
+
+const setup = async () => {
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    status: OrderStatus.Created,
+    userId: "asdf",
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  const data: ExpirationCompleteEvent["data"] = {
+    orderId: order.id,
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, order, ticket, data, msg };
+};
+
+it("", async () => {});
